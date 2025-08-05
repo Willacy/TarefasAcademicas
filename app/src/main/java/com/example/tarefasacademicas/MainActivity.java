0561fa4a8450be33894ad6675b78337d1e25ed42
Willacy <wannicy@gmail.com>
2025-08-05 20:39:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tarefasacademicas/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.tarefasacademicas;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.tarefasacademicas.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+
+        iniciarNavegacao();
+
+    }
+
+    private void iniciarNavegacao(){
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.menuNavegacaoInferior, navController);
+
+    }
+}
