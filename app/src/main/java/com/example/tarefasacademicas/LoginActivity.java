ce0b4afc2a314c2ac388dcfd96a78486e9f2cde3
Willacy <wannicy@gmail.com>
2025-08-22 07:38:23 -0300

    tela login

--- app/src/main/java/com/example/tarefasacademicas/LoginActivity.java
+++ app/src/main/java/com/example/tarefasacademicas/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.tarefasacademicas;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.tarefasacademicas.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(view -> login());
+
     }
+    private void login(){
+        // pega os dados do usuario
+        String user = binding.txtUser.getEditText().getText().toString();
+        String password = binding.txtPassword.getEditText().getText().toString();
+        // verifica se os dados não estao vazios
+        if(user.isEmpty()) {
+            binding.txtUser.setError("Campo vazio");
+        }else if(password.isEmpty()){
+            binding.txtPassword.setError("Campo vazio");
+        }else if(user.equals("admin") && password.equals("admin")){
+            Toast.makeText(this, "Login realizado com sucesso", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
