2d1b179d670b2a420133dc7152194291b272295c
Willacy <wannicy@gmail.com>
2025-08-22 22:52:07 -0300

    Tela de Splash criado

---
+++ app/src/main/java/com/example/tarefasacademicas/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.tarefasacademicas;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tarefasacademicas.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.ic_video_logo);
+        binding.splashVideo.setVideoURI(uri);
+
+        binding.splashVideo.setOnCompletionListener(mp -> {
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        });
+        binding.splashVideo.start();
+    }
+}
